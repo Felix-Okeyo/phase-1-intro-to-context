a3f177155b8fa8d859e784484267dec2ad53c12a
**** <****>
2023-07-10 18:31:52 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-// Your code here
-// Implementing the createEmployeeRecord function
-function createEmployeeRecord(array) {
+let dataEmployees = [
+    ["Thor", "Odinsson", "Electrical Engineer", 45],
+    ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+    ["Natalia", "Romanov", "CEO", 150],
+    ["Darcey", "Lewis", "Intern", 15],
+    ["Jarvis", "Stark", "CIO", 125],
+    ["Anthony", "Stark", "Angel Investor", 300],
+    ["Byron", "Poodle", "Mascot", 3],
+    ["Julius", "Caesar", "General", 27],
+    ["Rafiki", "", "Aide", 10],
+    ["Simba", "", "King", 100]
+  ];
+  
+  // Implementing the createEmployeeRecord function
+  function createEmployeeRecord(array) {
     let employeeRecord = {
       firstName: array[0],
       familyName: array[1],
